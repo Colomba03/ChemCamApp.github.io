cea6a67bf7f9201eb3235a93caa15cf0c3755d4f
Carlos Torres <carlos.torres62@upr.edu>
2022-03-19 14:25:51 -0400

    added navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./navbar.css";
+
+function Navbar() {
+  const pages = ['Home', 'Info', 'Description', 'Risk Management', 'Scope & Span'];
+
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default Navbar;

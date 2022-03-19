3d63a513bbf516e10f8ba08ea6a12756eb0bbaf8
Carlos Torres <carlos.torres62@upr.edu>
2022-03-19 14:23:54 -0400

    updated app js and added views css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+// import Navbar from './components/navbar';
+import Home from './views/Home';
+import Description from './views/DescriptivePart';
+import Information from './views/InformativePart';
+import Risks from './views/Risks';
+import ScopeSpan from './views/ScopeSpan';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Home />
     </div>
   );
 }

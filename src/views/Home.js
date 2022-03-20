edab21f7ed5be44ea9e6dd28475ada227db3af93
Carlos Torres <carlos.torres62@upr.edu>
2022-03-20 19:24:06 -0400

    added basic info to home page

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,11 +1,43 @@
+import { Container, Grid } from "@material-ui/core";
 import React from "react";
-import "./views.css"
+import "./views.css";
 
 function Home() {
   return (
-    <div className="text">
-      <h1>HOME PAGE</h1>
-    </div>
+    <Container className="homePage">
+      <Container className="generalInfo">
+        <h1 className="title">CHEMCAM</h1>
+        <body>
+          University of Puerto Rico, Mayagüez Campus <br />
+          Department of Computer Science and Engineering <br />
+          Software Requirements <br />
+          INSO 4115 <br /> <br />
+        </body>
+      </Container>
+      <Container>
+        <h2 className="members">Developers</h2>
+        <Grid container>
+          <Grid item xs={4}>
+            <Container className="fullStack">
+            <b>Frontend</b>
+            <li>Gabriela Cardona</li>
+            <li>Zaneisha Estrella</li>
+            <li>Génesis Torres</li>
+            <li>Ricardo Storm</li>
+            <li>Arnaldo Espinel</li>
+            </Container>
+          </Grid>
+          <Grid item xs={4}>
+            <Container className="fullStack">
+            <b>Backend</b>
+            <li>Luis Colomba</li>
+            <li>Bryan Torres</li>
+            <li>Carlos Torres</li>
+            </Container>
+          </Grid>
+        </Grid>
+      </Container>
+    </Container>
   );
 }
 

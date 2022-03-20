560245898261e470b99e434568946d9366d3cd3b
Carlos Torres <carlos.torres62@upr.edu>
2022-03-20 15:07:37 -0400

    fix home page path

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './views/Home';
-import Description from './views/DescriptivePart';
 import About from './views/About';
+import Description from './views/Description';
 import Risks from './views/Risks';
 import ScopeSpan from './views/ScopeSpan';
 
@@ -10,13 +10,13 @@ function App() {
   return (
     <Router>
       <Navbar />
-        <Link to="/"></Link>
+        <Link to="/chemcamapp.github.io"></Link>
         <Link to="/about"></Link>
         <Link to="/description"></Link>
         <Link to="/risks"></Link>
         <Link to="/scopespan"></Link>
         <Routes>
-          <Route exact path="/" element={<Home />} />
+          <Route exact path="/chemcamapp.github.io" element={<Home />} />
           <Route exact path="/about" element={<About />} />
           <Route exact path="/description" element={<Description />} />
           <Route exact path="/risks" element={<Risks />} />

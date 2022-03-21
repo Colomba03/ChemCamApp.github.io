5de4c4bdf1608fb920cd28a05a0a4900506fd1d3
Carlos Torres <carlos.torres62@upr.edu>
2022-03-21 00:21:17 -0400

    updated info

--- src/views/Home.js
+++ src/views/Home.js
@@ -15,10 +15,10 @@ function Home() {
         </div>
       </Container>
       <Container>
-        <h2 className="members">Developers</h2>
+        <h2 className="secHeader">Developers</h2>
         <Grid container>
           <Grid item xs={4}>
-            <Container className="fullStack">
+            <Container className="memberInfo">
             <b>Frontend</b>
             <li>Gabriela Cardona</li>
             <li>Zaneisha Estrella</li>
@@ -28,7 +28,7 @@ function Home() {
             </Container>
           </Grid>
           <Grid item xs={4}>
-            <Container className="fullStack">
+            <Container className="memberInfo">
             <b>Backend</b>
             <li>Luis Colomba</li>
             <li>Bryan Torres</li>
@@ -36,6 +36,17 @@ function Home() {
             </Container>
           </Grid>
         </Grid>
+      </Container>
+      <br /> <br />
+      <Container>
+        <h2 className="secHeader">Links</h2>
+        <div className="linkInfo">
+        <b>Github</b> <br />
+        <a href={"https://github.com/Colomba03/chemcamapp.github.io"}>https://github.com/Colomba03/chemcamapp.github.io</a>
+        <br /> <br />
+        <b>Trello</b> <br />
+        <a href={"https://trello.com/b/eo9RFLjW/team-6-chemcam"}>https://trello.com/b/eo9RFLjW/team-6-chemcam</a>
+        </div> <br />
       </Container>
     </Container>
   );
